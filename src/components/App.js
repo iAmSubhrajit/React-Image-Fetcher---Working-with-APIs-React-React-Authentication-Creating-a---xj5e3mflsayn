bdd6da8537063f71ca41f6a474848f5916381872
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-01-15 13:54:55 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+    const [value, setValue] = useState();
+    const [data, setData] = useState([])
+    const [loader, setLoader] = useState(false);
+    const handleInputValue = (e) => {
+        setValue(e.target.value)
+        if (value) {
+            setLoader(true)
+        }
+        else {
+            setLoader(false)
+        }
+    }
+    useEffect(() => {
+        axios.get(`https://jsonplaceholder.typicode.com/photos/${value}`)
+            .then((res) => {
+                setTimeout(() => {
+                    setData([res.data])
+                    setLoader(false)
+                });
+            })
+            .catch((error) => console.log(error))
+    }, [value])
+    return (
+        <>
+            Id number
+            <input type='number' onChange={handleInputValue} />
+
+            {loader ? <Loader /> : data.map((d) => (
+                <PhotoFrame url={d.url} title={d.title} />
+            ))}
+        </>
+    )
 }
 
 
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,12 @@
-import React from 'react';
 
-export const PhotoFrame = () => {
-   
+import React, { useEffect } from 'react';
+
+export const PhotoFrame = ({ url, title }) => {
+
+    return (
+        <div className='photoframe'>
+            <img src={url}></img>
+            <div className='caption'>{title}</div>
+        </div>
+    )
 }
